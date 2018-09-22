3041a261e50074a2f2c168b925d9d71d0e4936ad
oscar avila <oscaravilaperilla@hotmail.com>
2018-09-21 20:07:16 -0500

    d

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -10,15 +10,7 @@ export function loginUser(email, pass) {
     }
 }
 
-export function loginUser(email, pass) { 
-    return  dispatch => {
-     firebase.auth().signInWithEmailAndPassword(email, pass)
-         .then((resp) => {
-             return dispatch(loginUserSuccess(resp));
-         })
-         .catch((error) => { dispatch(loginUserFail)});
-     }
- }
+
 
 
 export const loginUserSuccess = (resp) => {
